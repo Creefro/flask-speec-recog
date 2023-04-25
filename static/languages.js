df64ebe4c886f751796dc6e29be8242f298fecd8
Mert Fazlı Saylak <76404240+merttfazli@users.noreply.github.com>
2023-04-25 18:54:11 +0300

    version 1.0.0

---
+++ static/languages.js
@@ -0,0 +1,15 @@
+const languages = [
+  {
+    no: "53",
+    name: "Turkish",
+    native: "Türkçe",
+    code: "tr",
+  },
+  {
+    no: "16",
+    name: "English",
+    native: "English",
+    code: "en",
+  },
+ 
+];
