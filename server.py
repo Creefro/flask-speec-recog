6ac1b2e8041a2fe11f63e11ab14beca87c6908d7
faegnir <71670295+faegnir@users.noreply.github.com>
2023-04-27 01:55:42 +0300

    speech recording and uploading works, with a nice frontend

--- server.py
+++ server.py
@@ -1,60 +1,132 @@
+import librosa
 from flask import *
 import pyaudio
 import wave
+import numpy as np
+import plotly.graph_objs as go
+import plotly.io as pio
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-	return render_template("upload.html")
+	return render_template("aa.html")
 
-@app.route('/success', methods = ['POST'])
+"""@app.route('/success', methods = ['POST'])
 def success():
 	if request.method == 'POST':
 		f = request.files['file']
 		f.save(f.filename)
 		return render_template("Acknowledgement.html", name = f.filename)
-        
+        """
+from glob import glob
 
-@app.route('/mic_record')
-def mic_record():
-        FRAMES_PER_BUFFER = 1024
-        FORMAT = pyaudio.paInt16
-        CHANNELS = 2
-        RATE = 16000
-        p = pyaudio.PyAudio()
-        
-        # starts recording
-        stream = p.open(
-                format=FORMAT,
-                channels=CHANNELS,
-                rate=RATE,
-                input=True,
-                frames_per_buffer=FRAMES_PER_BUFFER
-        )
+@app.route('/get_last_recorded_filename')
+def get_last_recorded_filename():
+    file_pattern = "record*.wav" # change this to match your file naming convention
+    files = glob(file_pattern)
+    if not files:
+        return ""
+    files = sorted(files, key=lambda f: int(os.path.splitext(f)[0][len("record"):]), reverse=True)
+    return files[0]
 
-        print("start recording...")
+@app.route('/<FUNCTION>')
+def command(FUNCTION=None):
+    print("")
+    exec(FUNCTION.replace("<br>", "\n"))
+    return ""
 
-        frames = []
-        seconds = 10
-        for i in range(0, int(RATE / FRAMES_PER_BUFFER * seconds)):
-                data = stream.read(FRAMES_PER_BUFFER)
-                frames.append(data)
+FRAMES_PER_BUFFER = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 16000
+import threading
+@app.route('/mic_record/<action>')
+def mic_record(action):
+    is_recording = False
+    global frames,stream
+    print(action)
+    if action == 'start':
+        if not is_recording:
+            
+            p = pyaudio.PyAudio()
 
-        print("recording stopped")
+            # starts recording
+            stream = p.open(
+                    format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=FRAMES_PER_BUFFER
+            )
 
-        stream.stop_stream()
-        stream.close()
-        p.terminate()
+            print("start recording...")
+            is_recording = True
+            frames = []
+
+            # start a new thread to continuously read frames from the stream and append to the frames list
+            def read_frames():
+                while is_recording:
+                    if stream.is_stopped():
+                        break
+                    data = stream.read(FRAMES_PER_BUFFER)
+                    frames.append(data)
+
+            threading.Thread(target=read_frames).start()
+
+    elif action == 'stop':
+            if is_recording:
+                print("recording stopped")
+                is_recording = False
+            p = pyaudio.PyAudio()
+
+            # stop the stream and terminate the PyAudio object
+            stream.stop_stream()
+            stream.close()
+            p.terminate()
+
+            # write the frames to a WAV file
+            recfilename = "record" + str(len(os.listdir())) + ".wav"
+            wf = wave.open(recfilename, 'wb')
+            wf.setnchannels(CHANNELS)
+            wf.setsampwidth(p.get_sample_size(FORMAT))
+            wf.setframerate(RATE)
+            wf.writeframes(b''.join(frames))
+            wf.close()
+
+    else:
+        print("not recording")
+    return ""
+
+@app.route('/success', methods = ['POST'])
+def success():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(f.filename)
+        waveform_html, spectrogram_html = generate_plots(f.filename)
+        return render_template("plots.html", name=f.filename, waveform_html=waveform_html, spectrogram_html=spectrogram_html)
 
 
-        wf = wave.open("asdqwe.wav", 'wb')
-        wf.setnchannels(CHANNELS)
-        wf.setsampwidth(p.get_sample_size(FORMAT))
-        wf.setframerate(RATE)
-        wf.writeframes(b''.join(frames))
-        wf.close()
-        return render_template('upload.html')
+def generate_plots(filename):
+    # Load audio file
+    y, sr = librosa.load(filename)
+
+    # Plot waveform
+    waveform_fig = go.Figure()
+    waveform_fig.add_trace(go.Scatter(x=np.arange(len(y))/sr, y=y))
+    waveform_fig.update_layout(title="Waveform", xaxis_title="Time (seconds)", yaxis_title="Amplitude")
+    waveform_html = pio.to_html(waveform_fig, full_html=False)
+
+    # Plot spectrogram
+    D = librosa.stft(y)
+    DB = librosa.amplitude_to_db(abs(D))
+    spectrogram_fig = go.Figure()
+    spectrogram_fig.add_trace(go.Heatmap(x=np.arange(DB.shape[1])/sr, y=np.arange(DB.shape[0]), z=DB))
+    spectrogram_fig.update_layout(title="Spectrogram", xaxis_title="Time (seconds)", yaxis_title="Frequency (Hz)")
+    spectrogram_html = pio.to_html(spectrogram_fig, full_html=False)
+
+    return waveform_html, spectrogram_html
 
 if __name__ == '__main__':
 	app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -1,91 +1,60 @@
-const recordBtn = document.querySelector(".record"),
-  result = document.querySelector(".result"),
-  downloadBtn = document.querySelector(".download"),
-  inputLanguage = document.querySelector("#language"),
-  clearBtn = document.querySelector(".clear");
+const recordBtn = document.querySelector(".record");
 
-let SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition,
-  recognition,
+
+
+let recording = false;
+
+function ExecPythonCommand(pythonCommand){
+  var request = new XMLHttpRequest()
+  request.open("GET", "/" + pythonCommand, true)
+  request.send()
+}
+
+function startRecording() {
+  ExecPythonCommand('mic_record/start');
+  recordBtn.classList.add("recording");
+  recordBtn.querySelector("p").innerHTML = "Listening...";
+  recording = true;
+}
+
+function stopRecording() {
+  ExecPythonCommand('mic_record/stop');
+  recordBtn.querySelector("p").innerHTML = "Start Listening";
+  recordBtn.classList.remove("recording");
   recording = false;
-let chunks = []; // chunks dizisi burada oluşturuluyor
-
-function populateLanguages() {
-  languages.forEach((lang) => {
-    const option = document.createElement("option");
-    option.value = lang.code;
-    option.innerHTML = lang.name;
-    inputLanguage.appendChild(option);
-  });
-}
-
-populateLanguages();
-
-function speechToText() {
-  try {
-    recognition = new SpeechRecognition();
-    recognition.lang = inputLanguage.value;
-    recognition.interimResults = true;
-    recordBtn.classList.add("recording");
-    recordBtn.querySelector("p").innerHTML = "Listening...";
-    recognition.start();
-    recognition.onresult = (event) => {
-      const speechResult = event.results[0][0].transcript;
-      //detect when intrim results
-      if (event.results[0].isFinal) {
-        result.innerHTML += " " + speechResult;
-        result.querySelector("p").remove();
-      } else {
-        //creative p with class interim if not already there
-        if (!document.querySelector(".interim")) {
-          const interim = document.createElement("p");
-          interim.classList.add("interim");
-          result.appendChild(interim);
+  var checkInterval = setInterval(function() {
+    var request = new XMLHttpRequest();
+    request.open("GET", "/get_last_recorded_filename", true);
+    request.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        var fileName = this.responseText;
+        if (fileName) {
+          fileNameDisplay.textContent = "File: " + fileName;
+          clearInterval(checkInterval);
         }
-        //update the interim p with the speech result
-        document.querySelector(".interim").innerHTML = " " + speechResult;
-      }
-      downloadBtn.disabled = false;
-      chunks.push(event.results[0][0].audioData); // kaydedilen sesi chunks dizisine ekliyoruz
-    };
-    recognition.onspeechend = () => {
-      speechToText();
-    };
-    recognition.onerror = (event) => {
-      stopRecording();
-      if (event.error === "no-speech") {
-        alert("No speech was detected. Stopping...");
-      } else if (event.error === "audio-capture") {
-        alert("No microphone was found. Ensure that a microphone is installed.");
-      } else if (event.error === "not-allowed") {
-        alert("Permission to use microphone is blocked.");
-      } else if (event.error === "aborted") {
-        alert("Listening Stopped.");
-      } else {
-        alert("Error occurred in recognition: " + event.error);
       }
     };
-  } catch (error) {
-    recording = false;
-
-    console.log(error);
-  }
+    request.send();
+  }, 500);
 }
+
+
+const fileInput = document.getElementById('file-input');
+const fileNameDisplay = document.getElementById('audioname');
 
 recordBtn.addEventListener("click", () => {
   if (!recording) {
-    speechToText();
-    recording = true;
+    startRecording();
   } else {
     stopRecording();
   }
 });
 
-function stopRecording() {
-  recognition.stop();
-  recordBtn.querySelector("p").innerHTML = "Start Listening";
-  recordBtn.classList.remove("recording");
-  recording = false;
-}
+
+  fileInput.addEventListener('change', function() {
+    fileNameDisplay.textContent = 'File: ' + this.files[0].name;
+  });
+
 
 function download() {
   const text = result.innerText;
